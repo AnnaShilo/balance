fc956e542c73e8a0f0537543ffe5c6639299724e
AnnaShilo <21.ann.shilo@gmail.com>
2020-09-17 00:01:37 +0300

    thrash added

--- Assets/Scenes/Script/playerController.cs
+++ Assets/Scenes/Script/playerController.cs
@@ -5,12 +5,38 @@ public class playerController : MonoBehaviour
 {
     public float speed;
 
+    public float maxspeed = 1.0f;
+
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+
+    /*
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3();
+
+        if (Physics.gravity.x != 0.0f)
+        {
+            movement = new Vector3(0.0f, moveHorizontal, moveVertical);
+        }
+        else if (Physics.gravity.y != 0.0f)
+        {
+            movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        }
+
+        rb.AddForce(movement * speed);
+
+        if (rb.velocity.magnitude > maxspeed)
+            rb.velocity = rb.velocity.normalized * maxspeed;
+    }
+    */
 
     void FixedUpdate()
     {
@@ -20,5 +46,8 @@ public class playerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+
+        if (rb.velocity.magnitude > maxspeed)
+            rb.velocity = rb.velocity.normalized * maxspeed;
     }
 }
